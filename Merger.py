8f1d968df0b1dec18208fc5dfb4980e6b7deeef2
DnyashA <chipa-vova@mail.ru>
2018-01-28 23:08:54 +0300

    Images merging + some minor changes

--- Merger.py
+++ Merger.py
@@ -4,14 +4,19 @@ from Splitter import Splitter
 import glob
 import os
 import numpy as np
+import shutil
 
 class Merger(object):
 
-    def __init__(self, main_dat_path, sub_dat_path, save_path, npy=False, shuffle=False, split=False):
+    def __init__(self, main_dat_path, sub_dat_path, mainmarkup, submarkup, save_path, npy=False, shuffle=False, split=False):
         # Path to standard dataset
         self.main_dataset_path = main_dat_path
         # Path to dataset which should be merged with standard
         self.sub_dataset_path = sub_dat_path
+        # Standard .csv file with markup
+        self.main_markup = mainmarkup
+        # .csv file which should be merged with standard
+        self.sub_markup = submarkup
         # Path to save merged dataset
         self.save_path = save_path
         # True for save dataset as NumPy archive (default False)
@@ -25,18 +30,30 @@ class Merger(object):
 
         if not os.path.exists(self.save_path):
             os.makedirs(str(self.save_path))
+        if not os.path.exists(str(self.save_path) + 'images/'):
+            os.makedirs(str(self.save_path) + 'images/')
+        if not os.path.exists(str(self.save_path) + 'csv_files/'):
+            os.makedirs(str(self.save_path) + 'csv_files/')
+
 
     def collect_csv(self):
-        with open(self.main_dataset_path) as maindata:
+        with open(str(self.main_dataset_path) + str(self.main_markup)) as maindata:
             # Delimiter have to be specified depending on csv content
             reader = csv.reader(maindata, delimiter=',')
             for row in reader:
                 self.main_data.append(row)
-        with open(self.sub_dataset_path) as subdata:
+        with open(str(self.sub_dataset_path) + str(self.sub_markup)) as subdata:
             # Delimiter have to be specified depending on csv content
             reader = csv.reader(subdata, delimiter=';')
             for row in reader:
                 self.sub_data.append(row)
+
+    def collect_ct(self):
+        # CT's extension have to be specified (usualy it's .raw or .dicom)
+        main_cts = glob.glob(str(self.main_dataset_path) + '**/*.raw', recursive=True)
+        sub_cts = glob.glob(str(self.sub_dataset_path) + '**/*.raw', recursive=True)
+        return main_cts, sub_cts
+
 
     def merge(self):
         main_dataset = pd.DataFrame(self.main_data[1:], columns=self.main_data[0])
@@ -44,12 +61,17 @@ class Merger(object):
         sub_dataset = pd.DataFrame(self.sub_data[1:], columns=self.sub_data[0])
         sub_dataset = sub_dataset.loc[:, ~sub_dataset.columns.duplicated()]
         result_dataset = main_dataset.append(sub_dataset, ignore_index=True)
-        pd.DataFrame.to_csv(result_dataset, str(self.save_path) + 'result_dataset.csv')
+        pd.DataFrame.to_csv(result_dataset, str(self.save_path) + 'csv_files/result_dataset.csv')
+        main_cts, sub_cts = self.collect_ct()
+        for path in main_cts:
+            shutil.copy(path, str(self.save_path) + 'images')
+        for path in sub_cts:
+            shutil.copy(path, str(self.save_path) + 'images')
 
         if self.shuffle:
             shuffled = pd.DataFrame(pd.DataFrame.as_matrix(result_dataset), columns=result_dataset.columns)
             np.random.shuffle(pd.DataFrame.as_matrix(shuffled))
-            pd.DataFrame.to_csv(shuffled, str(self.save_path) + 'result_dataset_shuffled.csv')
+            pd.DataFrame.to_csv(shuffled, str(self.save_path) + 'csv_files/result_dataset_shuffled.csv')
 
         if self.npy:
             result_dataset_npy = pd.DataFrame.as_matrix(result_dataset)
@@ -58,5 +80,5 @@ class Merger(object):
         if self.split:
             s = Splitter()
             train, test = s.split(0.1, pd.DataFrame.as_matrix(result_dataset), result_dataset.columns)
-            pd.DataFrame.to_csv(train, str(str(self.save_path) + 'result_dataset_train.csv'))
-            pd.DataFrame.to_csv(test, str(str(self.save_path) + 'result_dataset_test.csv'))
+            pd.DataFrame.to_csv(train, str(str(self.save_path) + 'csv_files/result_dataset_train.csv'))
+            pd.DataFrame.to_csv(test, str(str(self.save_path) + 'csv_files/result_dataset_test.csv'))
